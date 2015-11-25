946a1374a4e60c217fddccb09e749c7c337230e0
NewDatSci <linbaoqing@gmail.com>
2015-11-26 10:57:52 +1100

    first commit

---
+++ MvvmConcept/Model/FoodItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MvvmConcept.Model
+{
+    public class FoodItem: INotifyPropertyChanged
+    {
+
+        private string _foodname;
+        
+        public string FoodName
+        {
+            get
+            {
+                return _foodname;
+            }
+
+            set
+            {
+                if (_foodname == value)
+                {
+                    return;
+                }
+                _foodname = value;
+                RaisePropertyChanged("FoodName");
+            }
+        }
+
+        private double _itemprice;
+        public double ItemPrice
+        {
+            get
+            {
+                return _itemprice;
+            }
+
+            set
+            {
+                if (_itemprice == value)
+                {
+                    return;
+                }
+                _itemprice = value;
+                RaisePropertyChanged("ItemPrice");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MvvmConcept/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using MvvmConcept.Model;
+
+namespace MvvmConcept.ViewModel
+{
+    public class MainViewModel:INotifyPropertyChanged
+    {
+
+        private IFoodItemsService _dataService;
+
+        public ObservableCollection<FoodItem> FoodItems
+        {
+            get;
+            private set;
+        }
+
+        private 
+
+        public MainViewModel( IFoodItemsService dataService)
+        {
+            _dataService = dataService;
+            FoodItems = new ObservableCollection<FoodItem>();
+        }
+
+        private FoodItem _selectedFoodItem;
+
+        public FoodItem SelectedFoodItem
+        {
+            get
+            {
+                return _selectedFoodItem;
+            }
+            set
+            {
+                if(_selectedFoodItem == value)
+                {
+                    return;
+                }
+                _selectedFoodItem = value;
+                RaisePropertyChanged("SelectedFoodItem");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
